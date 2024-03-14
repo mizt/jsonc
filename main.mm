c838c77651b62a906805be74c674be4edd229871
mizt <info@mizt.org>
2024-03-14 15:55:45 +0900

    Initial commit

---
+++ main.mm
@@ -0,0 +1,45 @@
+#import <Foundation/Foundation.h>
+
+typedef struct _Params {
+	int quality = 100;
+} Params;
+
+class Settings {
+	
+	protected:
+	
+		Params params;
+	
+	private:
+		
+		bool isSameClassName(id a, NSString *b) { return (a&&[[a className] compare:b]==NSOrderedSame); }
+		bool isNumber(id a) { return isSameClassName(a,@"__NSCFNumber"); }
+		bool isBoolean(id a) { return isSameClassName(a,@"__NSCFBoolean"); }
+		bool isArray(id a) { return isSameClassName(a,@"__NSArrayM"); }
+		bool isMatrix(id a) { return (isSameClassName(a,@"__NSArrayM")&&[a count]==16); }
+		
+	public:
+	
+		Settings(NSString *path) {
+			NSString *jsonc = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+			NSMutableDictionary *settings = [[NSMutableDictionary alloc] init];
+			if(jsonc&&jsonc.length>0) {
+				settings = [NSJSONSerialization JSONObjectWithData:[[[NSRegularExpression regularExpressionWithPattern:@"(/\\*[\\s\\S]*?\\*/|//.*)" options:1 error:nil] stringByReplacingMatchesInString:jsonc options:0 range:NSMakeRange(0,jsonc.length) withTemplate:@""] dataUsingEncoding:NSUTF8StringEncoding] options:NSJSONReadingMutableContainers error:nil];
+				if(this->isNumber(settings[@"quality"])) this->params.quality = [settings[@"quality"] intValue];
+			}
+			NSLog(@"scale = %d",this->params.quality);
+		}
+};
+
+class App : public Settings {
+	
+	public:
+	
+		App(NSString *path):Settings(path) {}
+};
+
+int main(int argc, char *argv[]) {
+	@autoreleasepool {
+		new App(@"./settings.jsonc");
+	}
+}
